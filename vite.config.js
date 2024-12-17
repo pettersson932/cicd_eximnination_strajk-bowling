b94e7175944399533014ff808232bf5897aec3a2
pettersson932 <144891931+pettersson932@users.noreply.github.com>
2024-12-17 09:37:24 +0100

    x

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,30 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  test: {
+    environment: "jsdom",
+    globals: true,
+    setupFiles: "./setupTests.js",
+    coverage: {
+      provider: "v8",
+      reporter: ["text", "json", "lcov", "json-summary"],
+      include: ["src/**/*.js", "src/**/*.jsx"],
+      exclude: [
+        "src/main.jsx",
+        "src/App.jsx",
+        "src/router.jsx",
+        "src/views/Booking.jsx",
+        "src/views/Confirmation.jsx",
+        "src/components/BookingInfo/BookingInfo.jsx",
+        "src/components/ErrorMessage/ErrorMessage.jsx",
+        "src/components/Input/Input.jsx",
+        "src/components/Navigation/Navigation.jsx",
+        "src/components/Shoes/Shoes.jsx",
+        "src/components/Top/Top.jsx",
+      ],
+      reportsDirectory: "./coverage",
+    },
+  },
+});
