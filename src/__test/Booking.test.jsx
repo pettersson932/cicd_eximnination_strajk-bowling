0dfbc3eb21ea85f7eb13e2b8fe1cb7951f718be7
pettersson932 <144891931+pettersson932@users.noreply.github.com>
2024-12-19 00:41:35 +0100

    x

--- src/__test/Booking.test.jsx
+++ src/__test/Booking.test.jsx
@@ -40,13 +40,46 @@ describe("Booking", () => {
     expect(playersInput.value).toBe("2");
   });
 
-  it("reserve lanes based on the number of players", () => {
-    fireEvent.change(playersInput, { target: { value: "10" } });
+  it("user should be able to reserve lanes", () => {
+    fireEvent.change(playersInput, { target: { value: "4" } });
 
     if (lanesInput) {
-      fireEvent.change(lanesInput, { target: { value: "2" } });
-      expect(lanesInput.value).toBe("2");
+      fireEvent.change(lanesInput, { target: { value: "1" } });
+      expect(lanesInput.value).toBe("1");
     }
-    expect(playersInput.value).toBe("10");
+    expect(playersInput.value).toBe("4");
+  });
+
+  it("user should see errorMsg if too many players per lane in the booking", () => {
+    fireEvent.change(dateUserInput, { target: { value: "2024-12-31" } });
+    fireEvent.change(timeUserInput, { target: { value: "23:00" } });
+
+    expect(dateUserInput.value).toBe("2024-12-31");
+    expect(timeUserInput.value).toBe("23:00");
+
+    const addShoeButton = screen.getByText("+");
+    fireEvent.click(addShoeButton);
+    fireEvent.click(addShoeButton);
+    fireEvent.click(addShoeButton);
+    fireEvent.click(addShoeButton);
+    fireEvent.click(addShoeButton);
+
+    const shoeSizeInputs = screen.getAllByLabelText(/shoe size/i);
+    const shoeSizes = ["99", "99", "99", "99", "99"];
+
+    shoeSizes.forEach((size, index) => {
+      fireEvent.change(shoeSizeInputs[index], { target: { value: size } });
+      expect(shoeSizeInputs[index].value).toBe(size);
+    });
+
+    fireEvent.change(playersInput, { target: { value: "5" } });
+    fireEvent.change(lanesInput, { target: { value: "1" } });
+
+    const submitButton = screen.getByText(/strIIIIIike!/i);
+    fireEvent.click(submitButton);
+    const errorMessage = screen.getByText(
+      /det får max vara 4 spelare per bana/i
+    );
+    expect(errorMessage).toBeInTheDocument();
   });
 });
